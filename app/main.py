9d366619d2a32911258def6258ae1d28f48aaacb
Kapitoshka-a <taraspanechko737@gmail.com>
2024-07-03 14:53:51 +0300

    Cleaned with black

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
+origins = ["http://localhost:3000", "http://localhost", "http://localhost:8000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
+
+
 @app.get("/")
 async def root():
-    return {
-        "status_code": 200,
-        "detail": "ok",
-        "result": "working"
-    }
-
+    return {"status_code": 200, "detail": "ok", "result": "working"}
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -7,8 +7,4 @@ client = TestClient(app)
 def test_working():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "status_code": 200,
-        "detail": "ok",
-        "result": "working"
-    }
+    assert response.json() == {"status_code": 200, "detail": "ok", "result": "working"}
