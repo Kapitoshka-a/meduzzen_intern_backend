1e63a4f90690c57fe7d09512de9d15d4cee9ead9
Kapitoshka-a <taraspanechko737@gmail.com>
2024-07-09 11:29:16 +0300

    update user models

--- app/db/user_models.py
+++ app/db/user_models.py
@@ -7,11 +7,14 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id: int = Column(Integer, primary_key=True)
-    email: str = Column(String, nullable=False)
-    username: str = Column(String, nullable=False)
-    registered_at: datetime = Column(TIMESTAMP, default=datetime.utcnow)
-    role: str = Column(String, nullable=False, default="user")
+    email: str = Column(String, nullable=False, unique=True)
+    firstname: str = Column(String, nullable=False)
+    lastname: str = Column(String, nullable=False)
+    city: str = Column(String, nullable=False)
+    phone: str = Column(String, nullable=False)
     hashed_password: str = Column(String(length=1024), nullable=False)
+    created_at: datetime = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at: datetime = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
     is_verified: bool = Column(Boolean, default=False, nullable=False)
